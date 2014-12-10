2b7436022c2ec74e6ffa513b632fb510b55316a2
Ying Wang <wangying@google.com>
2014-12-10 12:44:33 -0800

    msm8909-common: Set PRODUCT_AAPT_PREBUILT_DPI
    The build system use both PRODUCT_AAPT_PREF_CONFIG and
    PRODUCT_AAPT_PREBUILT_DPI to select prebuilt dpi-specific apks.
    The first match takes precedence.
    
    scale: Configured for xhdpi by tetsuo55
    
    Change-Id: Iba98af754c3397ddbb27287244db086cb4c43aa6

--- msm8909.mk
+++ msm8909.mk
@@ -20,6 +20,7 @@ TARGET_BOARD_PLATFORM_VARIANT := msm8909
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
+PRODUCT_AAPT_PREBUILT_DPI := xhdpi 280dpi hdpi tvdpi mdpi ldpi
 
 # Device specific overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
