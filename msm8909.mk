63cf661085ed8b2725a44c276bbfeeaaab74ab50
Adrian DC <radian.dc@gmail.com>
2016-11-16 13:02:04 +0100

    msm8909-common: Increase ART heap growth limit to 192MB
     * Snapshot values exported from frameworks/native
        of the build/phone-xhdpi-1024-dalvik-heap.mk file:
    
        "Fix issue #7257947: Need to fix heap limits on Mako"
        Change-Id: Id8014b19195b8740bccf000ab66c1b599ba3a1a0
    
    From: Adrian DC <radian.dc@gmail.com>
    Date: Wed, 16 Nov 2016 13:02:04 +0100
    
        native: Increase ART heap limit to 192MB for 1024MB RAM devices
    
         * Needed for Google SetupWizard to start
    
         * Without this, SetupWizard is compiled by ART on boot
           and runs out of memory (heap) while running, resulting
           in the SetupWizard crashing and blocking boot
    
        Change-Id: Ic5b8caf4352f6d104af9121a09a01adad1c5bcc3
    
    Change-Id: Iee6766c3a912c0b841faa41c3d0776e87478817d
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- msm8909.mk
+++ msm8909.mk
@@ -43,13 +43,6 @@ PRODUCT_AAPT_PREF_CONFIG := xhdpi
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapgrowthlimit=128m \
-    dalvik.vm.heapminfree=6m \
-    dalvik.vm.heapstartsize=14m
-
-$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
-
 # Audio configuration
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/acdb/Bluetooth_cal.acdb:system/etc/Bluetooth_cal.acdb \
