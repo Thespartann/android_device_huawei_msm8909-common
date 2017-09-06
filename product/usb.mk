ca7b22b3dca70b998b4c91a11ffa74e7bc69477c
desalesouche <desalesouche@gmail.com>
2017-09-06 04:54:42 +0200

    msm8909-common: Fix MTP

--- product/usb.mk
+++ product/usb.mk
@@ -5,3 +5,15 @@ PRODUCT_PACKAGES += \
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml
+
+# USB ID
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.usb.id.midi=90BA \
+    ro.usb.id.midi_adb=90BB \
+    ro.usb.id.mtp=1051 \
+    ro.usb.id.mtp_adb=1052 \
+    ro.usb.id.ptp=1083 \
+    ro.usb.id.ptp_adb=1054 \
+    ro.usb.id.ums=1037 \
+    ro.usb.id.ums_adb=1038 \
+    ro.usb.vid=12d1
